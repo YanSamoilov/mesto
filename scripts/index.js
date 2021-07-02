1f8de78796e81c41259c83634bfb36a89fae20d4
Yan Samoilov <janchik31@yandex.ru>
2021-07-02 17:27:37 +0300

    Before First Review

--- scripts/index.js
+++ scripts/index.js
@@ -26,32 +26,32 @@ const cardAddPopupClassList = document.querySelector('#popup-add-card').classLis
 const initialCards = [
   {
     name: 'Большая голубая дыра',
-    link: '../images/bigBlue.JPG',
+    link: './images/bigBlue.jpg',
     alt: 'Темное круглое пятно в океане, означающее, что там глубоко.'
   },
   {
     name: 'Антарктида',
-    link: '../images/antarctica.jpg',
+    link: './images/antarctica.jpg',
     alt: 'Океан омывает ледяные горы.'
   },
   {
     name: 'Мадагаскар',
-    link: '../images/madagaskar.jpg',
+    link: './images/madagaskar.jpg',
     alt: 'Темнокожие женщины идут по проселочной дороге на фоне африканские деревья.'
   },
   {
     name: 'Амазонка',
-    link: '../images/amazonka.jpg',
+    link: './images/amazonka.jpg',
     alt: 'Из самолета видно извилистое русло реки в джунглях.'
   },
   {
     name: 'Большой барьерный риф',
-    link: '../images/Great-Barrier-Reef.jpg',
+    link: './images/Great-Barrier-Reef.jpg',
     alt: 'Извилистая длинная полоса рифов в воде окенана.'
   },
   {
     name: 'Под водой',
-    link: '../images/underwater.jpg',
+    link: './images/underwater.jpg',
     alt: 'Коралл под водой.'
   }
 ]
